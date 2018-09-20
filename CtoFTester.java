06ea7482996bea50583e072c252281e451d6b6ce
Michael Chan <mchan10@stuy.edu>
2018-09-19 20:14:46 -0400

    added fahrenheitToCelsius function

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,8 +1,12 @@
 public class CtoFTester{
 	public static void main(String[] args){
 		System.out.println(celsiusToFahrenheit(0));
+		System.out.println(fahrenheitToCelsius(32));
 	}
 	public static float celsiusToFahrenheit(float c){
 		return c * 9 / 5 + 32;
+	}
+	public static float fahrenheitToCelsius(float f){
+		return (f - 32) / 9 * 5;
 	}
 }
